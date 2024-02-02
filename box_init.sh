0f8908b3799d0f4deb83d86eec32d69c876ca2b1
CyberCherney <CyberSecCherney@proton.me>
2024-02-02 13:32:11 -0600

    added post skeleton script

---
+++ box_init.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Used for creating HTB markdown post shells and directories
+
+read -p "Enter the challenge name: " name
+read -p "Enter the box difficulty: " diff
+lower=`echo "${name,,}"`
+upper=`echo "${name^}"`
+
+dir="_activeboxes/$lower"
+
+mkdir $dir
+
+cat << header > $dir/-HTB_$upper.md
+---
+layout: post
+title: "HTB: $upper"
+author: Andrew Cherney
+date: 
+tags: htb $diff-box
+icon: "assets/icons/$lower.png"
+post_description: ""
+---
+
+# Summary
+
+{{ page.post_description }}
+
+# Enumeration
+
+
+header
+
+
